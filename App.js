02f31487e299cffbabf9768e755343d69438a3d5
RutujaLohar <rutujalohar88@gmail.com>
2024-02-10 20:02:13 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 
-
-const parent = React.createElement("div", { id: "parent"},
+    const parent = React.createElement("div", { id: "parent"},
     [React.createElement("div", { id: "child"},[
-        React.createElement("h1", {}, "I am an h1 tag"), 
-        React.createElement("h2", {}, "I am an h2 tag")
+        React.createElement("h1", {}, "This is Namaste React"), 
+        React.createElement("h2", {}, "I am Rutuja")
     ]),
     React.createElement("div", { id: "child2"},[
         React.createElement("h1", {}, "I am an h1 tag"), 
@@ -11,7 +12,7 @@ const parent = React.createElement("div", { id: "parent"},
     ]),
 ]);
 
-    console.log(parent);
+    console.log(parent);//root
 
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
---
+++ README.md
@@ -0,0 +1,22 @@
+# Namaste React 
+
+
+
+# parcel
+-Dev Build
+-Local server
+-HMR = Hot Module Replacement
+-File Watching Algorithm = written in C++
+-Catching - Faster Builds
+-Image Optimization
+-Minification
+-Bundling
+-Compress
+-Consistent Hashing
+-Code splitting
+-Differential bundling
+-Diagnostic
+-Error Handling
+-HTTPs
+-Trees Shaking - remove unsend code
+Differentdev and production bundles
